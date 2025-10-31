16b355ddd3a4c234f00c04d6b5aca2d2b50a438c
AB00R <blib645@gmail.com>
2025-10-31 11:01:41 +0100

    add placeholder data

---
+++ lib/data/recipe_data.dart
@@ -0,0 +1,53 @@
+class Recipe {
+  final String name;
+  final String category;
+  final int duration;
+  final bool isReady;
+  final String imagePath;
+
+  Recipe({
+    required this.name,
+    required this.category,
+    required this.duration,
+    required this.isReady,
+    required this.imagePath,
+  });
+}
+
+final List<Recipe> traditionalRecipes = [
+  Recipe(
+    name: 'Couscous',
+    category: 'Traditional',
+    duration: 120,
+    isReady: true,
+    imagePath: 'assets/images/recipes/couscous.png',
+  ),
+  Recipe(
+    name: 'Hrira',
+    category: 'Traditional',
+    duration: 60,
+    isReady: true,
+    imagePath: 'assets/images/recipes/hrira.png',
+  ),
+  Recipe(
+    name: 'Rogag',
+    category: 'Traditional',
+    duration: 120,
+    isReady: true,
+    imagePath: 'assets/images/recipes/roqaq.png',
+  ),
+  Recipe(
+    name: 'Tagine',
+    category: 'Traditional',
+    duration: 60,
+    isReady: false,
+    imagePath: 'assets/images/recipes/tagine.png',
+  ),
+  Recipe(
+    name: 'Bastilla',
+    category: 'Traditional',
+    duration: 40,
+    isReady: true,
+    imagePath: 'assets/images/recipes/bastilla.png',
+  ),
+];
