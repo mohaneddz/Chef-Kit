9a63b5b1006340bfe15787582c264481ca366ac8
SalahBadreddine <salah.badreddine@ensia.edu.dz>
2025-10-31 09:34:10 +0100

    fix: resolved merge issues in inventory page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chefkit/views/pages/inventory_page.dart';
 import 'package:chefkit/views/pages/item_page.dart';
 // import 'package:chefkit/views/pages/login_page.dart';
 import 'package:flutter/material.dart';
@@ -14,7 +15,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: "Poppins"),
-      home: ItemPage(),
+      home: InventoryPage(),
     );
   }
 }
