48af480aedfb0ea6fd9a68047a4b9d6c917b4fb1
SalahBadreddine <salah.badreddine@ensia.edu.dz>
2025-12-04 17:14:38 +0100

    fix: added navigation

--- lib/views/screens/authentication/login_page.dart
+++ lib/views/screens/authentication/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:chefkit/blocs/auth/auth_cubit.dart';
 import 'package:chefkit/common/constants.dart';
 import 'package:chefkit/views/screens/authentication/singup_page.dart';
+import 'package:chefkit/views/screens/home_page.dart';
 import 'package:chefkit/views/widgets/button_widget.dart';
 import 'package:chefkit/views/widgets/text_field_widget.dart';
 import 'package:chefkit/views/layout/triangle_painter.dart';
@@ -21,131 +22,138 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     final errors = context.watch<AuthCubit>().state.fieldErrors;
-    return Scaffold(
-      backgroundColor: AppColors.red600,
-      body: Stack(
-        alignment: Alignment.topCenter,
-        children: [
-          CustomPaint(
-            size: Size(
-              MediaQuery.of(context).size.width,
-              MediaQuery.of(context).size.height,
+    return BlocListener<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (!state.loading && state.user != null && state.fieldErrors.isEmpty) {
+          Navigator.pushReplacement(context, MaterialPageRoute(builder:(context) => const HomePage(),));
+        }
+      },
+      child: Scaffold(
+        backgroundColor: AppColors.red600,
+        body: Stack(
+          alignment: Alignment.topCenter,
+          children: [
+            CustomPaint(
+              size: Size(
+                MediaQuery.of(context).size.width,
+                MediaQuery.of(context).size.height,
+              ),
+              painter: TrianglePainter(
+                color: AppColors.orange,
+                stratProportion: 0.4,
+                endProportion: 0.2,
+              ),
             ),
-            painter: TrianglePainter(
-              color: AppColors.orange,
-              stratProportion: 0.4,
-              endProportion: 0.2,
-            ),
-          ),
-          LayoutBuilder(
-            builder: (context, constraints) => SingleChildScrollView(
-              child: ConstrainedBox(
-                constraints: BoxConstraints(
-                  minHeight: MediaQuery.of(context).size.height,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 20,
-                    vertical: 20,
+            LayoutBuilder(
+              builder: (context, constraints) => SingleChildScrollView(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    minHeight: MediaQuery.of(context).size.height,
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        children: [
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height * 0.13,
-                          ),
-                          Image.asset(
-                            "assets/images/couscous.png",
-                            width: 180,
-                          ),
-                          SizedBox(height: 20),
-                          Text(
-                            "Login",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 32,
-                              fontWeight: FontWeight.w600,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 20,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.13,
                             ),
-                          ),
-                          SizedBox(height: 41),
-                          // the icons will be changed
-                          TextFieldWidget(
-                            controller: emailController,
-                            hintText: "Email Address",
-                            trailingIcon: Icons.email_outlined,
-                            errorText: errors["email"],
-                          ),
-                          SizedBox(height: 20),
-                          TextFieldWidget(
-                            controller: passwordController,
-                            hintText: "Password",
-                            trailingIcon: Icons.visibility_off_outlined,
-                            isPassword: true,
-                            errorText: errors["password"],
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          ButtonWidget(
-                            text: "Log In",
-                            onTap: () {
-                              context.read<AuthCubit>().login(
-                                emailController.text.trim(),
-                                passwordController.text.trim(),
-                              );
-                            },
-                          ),
-                          SizedBox(height: 25),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                "Forgot email or password ?",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w400,
-                                ),
+                            Image.asset(
+                              "assets/images/couscous.png",
+                              width: 180,
+                            ),
+                            SizedBox(height: 20),
+                            Text(
+                              "Login",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 32,
+                                fontWeight: FontWeight.w600,
                               ),
-                              SizedBox(width: 3),
-                              TextButton(
-                                onPressed: () => Navigator.pushReplacement(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => const SingupPage(),
-                                  ),
-                                ),
-                                style: TextButton.styleFrom(
-                                  padding: EdgeInsets.zero,
-                                  minimumSize: Size.zero,
-                                  tapTargetSize:
-                                      MaterialTapTargetSize.shrinkWrap,
-                                  visualDensity: VisualDensity.compact,
-                                ),
-                                child: Text(
-                                  "Sing Up",
+                            ),
+                            SizedBox(height: 41),
+                            // the icons will be changed
+                            TextFieldWidget(
+                              controller: emailController,
+                              hintText: "Email Address",
+                              trailingIcon: Icons.email_outlined,
+                              errorText: errors["email"],
+                            ),
+                            SizedBox(height: 20),
+                            TextFieldWidget(
+                              controller: passwordController,
+                              hintText: "Password",
+                              trailingIcon: Icons.visibility_off_outlined,
+                              isPassword: true,
+                              errorText: errors["password"],
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            ButtonWidget(
+                              text: "Log In",
+                              onTap: () {
+                                context.read<AuthCubit>().login(
+                                  emailController.text.trim(),
+                                  passwordController.text.trim(),
+                                );
+                              },
+                            ),
+                            SizedBox(height: 25),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Forgot email or password ?",
                                   style: TextStyle(
+                                    color: Colors.white,
                                     fontSize: 14,
-                                    fontWeight: FontWeight.bold,
-                                    color: Color(0xFFFFA974),
+                                    fontWeight: FontWeight.w400,
                                   ),
                                 ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 15),
-                        ],
-                      ),
-                    ],
+                                SizedBox(width: 3),
+                                TextButton(
+                                  onPressed: () => Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => const SingupPage(),
+                                    ),
+                                  ),
+                                  style: TextButton.styleFrom(
+                                    padding: EdgeInsets.zero,
+                                    minimumSize: Size.zero,
+                                    tapTargetSize:
+                                        MaterialTapTargetSize.shrinkWrap,
+                                    visualDensity: VisualDensity.compact,
+                                  ),
+                                  child: Text(
+                                    "Sing Up",
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color(0xFFFFA974),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 15),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/views/screens/authentication/singup_page.dart
+++ lib/views/screens/authentication/singup_page.dart
@@ -1,6 +1,7 @@
 import 'package:chefkit/blocs/auth/auth_cubit.dart';
 import 'package:chefkit/common/constants.dart';
 import 'package:chefkit/views/screens/authentication/login_page.dart';
+import 'package:chefkit/views/screens/home_page.dart';
 import 'package:chefkit/views/widgets/button_widget.dart';
 import 'package:chefkit/views/widgets/text_field_widget.dart';
 import 'package:chefkit/views/layout/triangle_painter.dart';
@@ -24,149 +25,156 @@ class _SingupPageState extends State<SingupPage> {
   @override
   Widget build(BuildContext context) {
     final errors = context.watch<AuthCubit>().state.fieldErrors;
-    return Scaffold(
-      backgroundColor: AppColors.red600,
-      body: Stack(
-        alignment: Alignment.topCenter,
-        children: [
-          CustomPaint(
-            size: Size(
-              MediaQuery.of(context).size.width,
-              MediaQuery.of(context).size.height,
+    return BlocListener<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (!state.loading && state.user != null && state.fieldErrors.isEmpty) {
+          Navigator.pushReplacement(context, MaterialPageRoute(builder:(context) => const HomePage(),));
+        }
+      },
+      child: Scaffold(
+        backgroundColor: AppColors.red600,
+        body: Stack(
+          alignment: Alignment.topCenter,
+          children: [
+            CustomPaint(
+              size: Size(
+                MediaQuery.of(context).size.width,
+                MediaQuery.of(context).size.height,
+              ),
+              painter: TrianglePainter(
+                color: AppColors.orange,
+                stratProportion: 0.35,
+                endProportion: 0.15,
+              ),
             ),
-            painter: TrianglePainter(
-              color: AppColors.orange,
-              stratProportion: 0.35,
-              endProportion: 0.15,
-            ),
-          ),
-          LayoutBuilder(
-            builder: (context, constraints) {
-              return SingleChildScrollView(
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(
-                    minHeight: constraints.maxHeight,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 20,
-                      vertical: 20,
+            LayoutBuilder(
+              builder: (context, constraints) {
+                return SingleChildScrollView(
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                      minHeight: constraints.maxHeight,
                     ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          children: [
-                            SizedBox(
-                              height:
-                                  MediaQuery.of(context).size.height * 0.10,
-                            ),
-                            Image.asset(
-                              "assets/images/couscous.png",
-                              width: 180,
-                            ),
-                            const SizedBox(height: 20),
-                            const Text(
-                              "Sign Up",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 32,
-                                fontWeight: FontWeight.w600,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 20,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            children: [
+                              SizedBox(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.10,
                               ),
-                            ),
-                            const SizedBox(height: 41),
-                            TextFieldWidget(
-                              controller: fullnameController,
-                              hintText: "Full Name",
-                              trailingIcon: Icons.person_outline,
-                              errorText: errors["name"],
-                            ),
-                            const SizedBox(height: 20),
-                            TextFieldWidget(
-                              controller: emailController,
-                              hintText: "Email Address",
-                              trailingIcon: Icons.email_outlined,
-                              errorText: errors["email"],
-                            ),
-                            const SizedBox(height: 20),
-                            TextFieldWidget(
-                              controller: passwordController,
-                              hintText: "Password",
-                              trailingIcon: Icons.visibility_off_outlined,
-                              isPassword: true,
-                              errorText: errors["password"],
-                            ),
-                            const SizedBox(height: 20),
-                            TextFieldWidget(
-                              controller: confirmPasswordController,
-                              hintText: "Confirm Password",
-                              trailingIcon: Icons.visibility_off_outlined,
-                              isPassword: true,
-                              errorText: errors["confirm"],
-                            ),
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            const SizedBox(height: 30),
-                            ButtonWidget(
-                              text: "Sign Up",
-                              onTap: () {
-                                context.read<AuthCubit>().signup(
-                                  fullnameController.text.trim(),
-                                  emailController.text.trim(),
-                                  passwordController.text.trim(),
-                                  confirmPasswordController.text.trim(),
-                                );
-                              },
-                            ),
-                            const SizedBox(height: 25),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                const Text(
-                                  "Already have an account?",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 14,
-                                  ),
+                              Image.asset(
+                                "assets/images/couscous.png",
+                                width: 180,
+                              ),
+                              const SizedBox(height: 20),
+                              const Text(
+                                "Sign Up",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.w600,
                                 ),
-                                const SizedBox(width: 3),
-                                TextButton(
-                                  onPressed: () => Navigator.pushReplacement(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => const LoginPage(),
-                                    ),
-                                  ),
-                                  style: TextButton.styleFrom(
-                                    padding: EdgeInsets.zero,
-                                    minimumSize: Size.zero,
-                                    tapTargetSize:
-                                        MaterialTapTargetSize.shrinkWrap,
-                                  ),
-                                  child: const Text(
-                                    "Login",
+                              ),
+                              const SizedBox(height: 41),
+                              TextFieldWidget(
+                                controller: fullnameController,
+                                hintText: "Full Name",
+                                trailingIcon: Icons.person_outline,
+                                errorText: errors["name"],
+                              ),
+                              const SizedBox(height: 20),
+                              TextFieldWidget(
+                                controller: emailController,
+                                hintText: "Email Address",
+                                trailingIcon: Icons.email_outlined,
+                                errorText: errors["email"],
+                              ),
+                              const SizedBox(height: 20),
+                              TextFieldWidget(
+                                controller: passwordController,
+                                hintText: "Password",
+                                trailingIcon: Icons.visibility_off_outlined,
+                                isPassword: true,
+                                errorText: errors["password"],
+                              ),
+                              const SizedBox(height: 20),
+                              TextFieldWidget(
+                                controller: confirmPasswordController,
+                                hintText: "Confirm Password",
+                                trailingIcon: Icons.visibility_off_outlined,
+                                isPassword: true,
+                                errorText: errors["confirm"],
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              const SizedBox(height: 30),
+                              ButtonWidget(
+                                text: "Sign Up",
+                                onTap: () {
+                                  context.read<AuthCubit>().signup(
+                                    fullnameController.text.trim(),
+                                    emailController.text.trim(),
+                                    passwordController.text.trim(),
+                                    confirmPasswordController.text.trim(),
+                                  );
+                                },
+                              ),
+                              const SizedBox(height: 25),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Text(
+                                    "Already have an account?",
                                     style: TextStyle(
+                                      color: Colors.white,
                                       fontSize: 14,
-                                      fontWeight: FontWeight.bold,
-                                      color: Color(0xFFFFA974),
                                     ),
                                   ),
-                                ),
-                              ],
-                            ),
-                            const SizedBox(height: 15),
-                          ],
-                        ),
-                      ],
+                                  const SizedBox(width: 3),
+                                  TextButton(
+                                    onPressed: () => Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => const LoginPage(),
+                                      ),
+                                    ),
+                                    style: TextButton.styleFrom(
+                                      padding: EdgeInsets.zero,
+                                      minimumSize: Size.zero,
+                                      tapTargetSize:
+                                          MaterialTapTargetSize.shrinkWrap,
+                                    ),
+                                    child: const Text(
+                                      "Login",
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                        color: Color(0xFFFFA974),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 15),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              );
-            },
-          ),
-        ],
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
