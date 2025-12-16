543b98265bd06f8439d81c46bc15999415b44e94
Badro <badro.zerraf05@gmail.com>
2025-12-16 04:34:58 +0100

    revert to backend host

--- lib/common/config.dart
+++ lib/common/config.dart
@@ -1,9 +1,9 @@
 class AppConfig {
   // Production:
-  // static const String baseUrl = 'https://chefkit-backend-ss6c.onrender.com';
+  static const String baseUrl = 'https://chefkit-backend-ss6c.onrender.com';
 
   // Local development (Android emulator uses 10.0.2.2 for host machine):
-  static const String baseUrl = 'http://10.0.2.2:8000';
+  // static const String baseUrl = 'http://10.0.2.2:8000';
 
   // For physical device, use your computer's IP (e.g., 'http://192.168.1.100:8000')
 }
